08167808aafe5db9978aa7c6ab868ae3aa212e5b
martinnnajiofor <martinnnajiofor@yahoo.com>
2023-02-16 01:42:24 -0800

    0x01. NoSQL

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -5,4 +5,4 @@ def schools_by_topic(mongo_collection, topic):
     """function that returns the list of school having a specific topic"""
     query = { "topics": topic }
     schoolst = mongo_collection.find(query)
-    return 
+    return schoolst 
